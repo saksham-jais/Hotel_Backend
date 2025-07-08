e36d45a2b0416460d78e48ab71996e72380634ef
Saksham Jaiswal <sakshamjais100@gmail.com>
2025-07-08 19:23:38 +0530

    Shift MongoDB To Online Server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const db = require('./db');
+require('dotenv').config();
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json())
@@ -14,5 +15,5 @@ app.use('/person',personRoutes);
 
 const menuRoutes = require('./routes/menuRoutes');
 app.use('/menuitem',menuRoutes);
-
-app.listen(3000, () => { console.log("Server is Running On Port 3000") })
+const PORT=process.env.PORT || 3000;
+app.listen(PORT, () => { console.log("Server is Running On Port 3000") })
